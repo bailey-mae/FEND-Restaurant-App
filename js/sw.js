04f653b552a3e8c68b0500978ed36bade7a79aae
Laura Bailey <lauramginn@gmail.com>
2018-10-15 13:46:25 -0400

    Service worker: updated sw.js to include cache in install function

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-const cacheFiles = [
+/*const cacheFiles = [
 '/',
 '/index.html',
 '/restaurant.html',
@@ -17,12 +17,31 @@ const cacheFiles = [
 '/img/8.jpg',
 '/img/9.jpg',
 '/img/10.jpg',
-];
+];*/
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('v1').then(function(cache) {
-            return cache.addAll(cacheFiles);
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/restaurant.html',
+                '/css/styles.css',
+                '/js/dbhelper.js',
+                'js/main.js',
+                '/js/restaurant_info.js',
+                '/data/restaurants.json',
+                '/img/1.jpg',
+                '/img/2.jpg',
+                '/img/3.jpg',
+                '/img/4.jpg',
+                '/img/5.jpg',
+                '/img/6.jpg',
+                '/img/7.jpg',
+                '/img/8.jpg',
+                '/img/9.jpg',
+                '/img/10.jpg',
+            ]);
         })
     );
 });
@@ -46,5 +65,5 @@ self.addEventListener('fetch', function(e) {
                 })
                 .catch(function(err) {
                     console.error(err);
-        });
+        })
 }
