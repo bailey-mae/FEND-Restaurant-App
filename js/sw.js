d795ba9f04566b232bfb3455d0d83851ed2eb5fa
Laura Bailey <lauramginn@gmail.com>
2018-10-16 11:03:28 -0400

    Remove unnecessary comments from sw.js file

--- js/sw.js
+++ js/sw.js
@@ -1,24 +1,3 @@
-/*const cacheFiles = [
-'/',
-'/index.html',
-'/restaurant.html',
-'/css/styles.css',
-'/js/dbhelper.js',
-'js/main.js',
-'/js/restaurant_info.js',
-'/data/restaurants.json',
-'/img/1.jpg',
-'/img/2.jpg',
-'/img/3.jpg',
-'/img/4.jpg',
-'/img/5.jpg',
-'/img/6.jpg',
-'/img/7.jpg',
-'/img/8.jpg',
-'/img/9.jpg',
-'/img/10.jpg',
-];*/
-
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('v1').then(function(cache) {
@@ -46,27 +25,6 @@ self.addEventListener('install', function(e) {
     );
 });
 
-/*self.addEventListener('fetch', function(e) {
-    e.respondWith(
-        caches.match(e.request).then(function(response){
-            if (response) {
-                console.log('Found', e.request, ' in cache');
-                return response;
-            }
-            else {
-                console.log('Could not find ', e.request, ' in cache, Fetching!');
-                return fetch(e.request)
-                .then(function(response) {
-                    const clonedResponse = response.clone();
-                    caches.open('v1').then(function(cache) {
-                        cache.put(e.request, clonedResponse);
-                    });
-                    return response;
-                })
-                .catch(function(err) {
-                    console.error(err);
-        })
-}*/
 
 /* resource: https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker*/
 self.addEventListener("fetch", function(event) {
