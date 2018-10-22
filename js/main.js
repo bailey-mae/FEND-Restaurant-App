50b32948fa44bfab564ff90774a1a1c6d7391430
Laura Bailey <lauramginn@gmail.com>
2018-10-22 14:46:51 -0400

    Navigation: edit tab index

--- js/main.js
+++ js/main.js
@@ -178,7 +178,6 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  name.setAttribute('tabindex', '0');
   li.append(name);
 
   const neighborhood = document.createElement('p');
