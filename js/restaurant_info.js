4d1188ec53ab614bca9bfdb319a564e1a11037bf
Laura Bailey <lauramginn@gmail.com>
2018-10-22 14:39:21 -0400

    Accessibility: added alt img values and aria label/role to js and html files

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -88,6 +88,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.alt = `${restaurant.name} Restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
